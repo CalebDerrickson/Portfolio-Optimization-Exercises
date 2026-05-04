b41ef9145a1d7319f8c44f0188244047399b9e85
Caleb Derrickson <Calebderrickson@gmail.com>
2026-05-03 21:16:16 -0500

    Exercise 1. Y axis on log scale doesn't work, who cares.

--- Chapter2/Exercises.py
+++ Chapter2/Exercises.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import matplotlib
+from typing import Callable
+import datetime
+import matplotlib.ticker as mticker
 
 PATH_TO_STORED_DATA: list[Path] = list((Path().cwd().resolve() / "Historical Data").glob("*.csv"))
 STORED_DATA_NAMES = [data_path.name.split(" ")[0] for data_path in PATH_TO_STORED_DATA]
@@ -26,13 +30,34 @@ def get_data(stock_names: list[str] | str = "") -> dict[str, pd.DataFrame]:
         res[stock_name_to_get] = pd.read_csv(data_path)
 
     return res
-        
+
+
+def Exercise_1():
+    data: pd.DataFrame = get_data("APPL").get("APPL")[["Date", "Close/Last"]]
+    dates: np.ndarray = np.array([datetime.datetime.strptime(date, "%m/%d/%Y") for date in data["Date"].to_numpy()])
+    stock_data: np.ndarray = pd.to_numeric(data["Close/Last"].str.removeprefix("$")).to_numpy()
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1, layout="constrained")
+    ax1.plot(dates, stock_data)
+    ax1.set(title="Closing Price (Linear Scale)")
+    ax1.grid()
+    ax1.grid(which="minor", color="0.9")
+    ax1.xaxis.set_major_locator(matplotlib.dates.YearLocator())
+
+    ax2.semilogy(dates, stock_data)
+    ax2.set(title="Closing Price (Log Scale)")
+    ax2.grid()
+    ax2.grid(which="minor", color="0.9")
+    ax2.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.2f'))
+    ax2.xaxis.set_major_locator(matplotlib.dates.YearLocator())
+    fig.savefig("Chapter2/Plots/APPL Closing Price Exercise 2.1.png")
+
 
 def main():
-    stock_names = []
-
-    stock_data: dict[str, pd.DataFrame] = get_data()
-    print(stock_data.keys())
+    exercises: list[Callable] = [Exercise_1]
+    for exercise in exercises:
+        print(exercise.__name__)
+        exercise()
 
 
 if __name__ == "__main__":
